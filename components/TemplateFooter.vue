53d7f4a136a062b68fa647aade1223b1e65773f8
milan <milan.herbe@gmail.com>
2024-09-30 17:42:25 +0200

    fix: last update

--- components/TemplateFooter.vue
+++ components/TemplateFooter.vue
@@ -28,9 +28,9 @@
 
           <div class="mt-6 inline-flex items-center space-x-2">
             <div
-              class="relative flex items-center justify-center h-5 w-5 rounded-full bg-[#BFFFBF] lg:h-6 lg:w-6"
+              class="relative flex items-center justify-center h-5 w-5 rounded-full bg-[#BFFFBF] lg:h-6 lg:w-6 "
             >
-              <div class="h-2 w-2 rounded-full bg-[#00FF00]"></div>
+              <div class="h-2 w-2 rounded-full bg-[#00FF00] animate-ping"></div>
             </div>
             <span
               class="text-lg font-regular text-neutral-900 lg:text-xl dark:text-gray-50"
