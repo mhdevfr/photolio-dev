b84876a3fcc860a1bd8682b252ba0a5d72181b4b
milan <milan.herbe@gmail.com>
2024-10-01 23:20:11 +0200

    fix: dark preselected colorMode

--- middleware/resetColorMod.ts
+++ middleware/resetColorMod.ts
@@ -1,10 +1,7 @@
 import { useColorMode } from '@vueuse/core';
-
 export default defineNuxtRouteMiddleware((to, from) => {
   const colorMode = useColorMode();
-
-  // Réinitialiser le mode à "light" sur certaines pages (par exemple, landing ou login)
   if (to.path === '/' || to.path === '/login' || to.path === '/signup') {
-    colorMode.preference = 'light'; // Force le mode clair
+    colorMode.preference = 'dark'; 
   }
 });
