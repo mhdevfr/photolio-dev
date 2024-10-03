d8f1d4d4ce63a732ed1288164c87b8850c2ba835
milan <milan.herbe@gmail.com>
2024-10-03 12:24:01 +0200

    style: updated and usehead

--- components/templateDarkBtn.vue
+++ components/templateDarkBtn.vue
@@ -1,22 +1,22 @@
 <template>
   <button
     color="gray"
-    class="dark:text-white text-gray-800 brico-800 text-start flex items-center justify-end p-3 w-full"
+    class="dark:text-white text-gray-800 brico-800 text-start flex items-center justify-start p-3 w-full"
     variant="ghost"
     aria-label="Theme"
     @click="isDark = !isDark"
     v-motion-fade-visible
   >
-    <Icon :name="isDark ? 'i-heroicons-sun-20-solid' : 'i-heroicons-moon-20-solid'"
+    <Icon
+      :name="isDark ? 'game-icons:striped-sun' : 'i-heroicons-moon-20-solid'"
       class=""
-      size="24"
+      size="32"
     />
 
-  
 
-    <span class="ml-2 text-sm font-medium brico-800">{{
+<span class="ml-2 text-sm font-medium brico-800">{{
       isDark ? "Light" : "Dark"
-    }}</span>
+    }}</span> 
   </button>
 </template>
 <script setup lang="ts">
